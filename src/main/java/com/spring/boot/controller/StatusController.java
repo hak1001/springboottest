72be1f94db575e941c2c767c92d97ea1025f26ab
hak1001 <sr1218zz@gmail.com>
2019-01-07 23:05:04 +0900

    add POST test

--- src/main/java/com/spring/boot/controller/StatusController.java
+++ src/main/java/com/spring/boot/controller/StatusController.java
@@ -5,6 +5,8 @@ import java.util.Enumeration;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +18,23 @@ public class StatusController {
     @RequestMapping(value = "/ping", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public String isRunning(HttpServletRequest request) {
+    	System.out.println("GET");
+    	Enumeration<String> em = request.getHeaderNames();
+    	 
+        while(em.hasMoreElements()){
+            String name = em.nextElement() ;
+            String val = request.getHeader(name) ;
+            System.out.println(name + " : " + val) ;
+        }
+
+        return "I'm Alive!";
+    }
+    
+//    @RequestMapping(value = "/ping", method = RequestMethod.GET)
+    @PostMapping(value = "/ping")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String isRunning2(HttpServletRequest request, @RequestBody String txt) {
+    	System.out.println("POST : " + txt);
     	Enumeration<String> em = request.getHeaderNames();
     	 
         while(em.hasMoreElements()){
--- src/test/java/com/spring/boot/RestTemplateTest.java
+++ src/test/java/com/spring/boot/RestTemplateTest.java
@@ -17,10 +17,10 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootTest
 public class RestTemplateTest {
 	RestTemplate restTemplate = new RestTemplate();
+	private static final String url = "http://127.0.0.1:8080/ping";
 	
-	@Test
-	public void restTest() {
-		String url = "http://127.0.0.1:8080/ping";
+//	@Test
+	public void getTest() {
 		String result = restTemplate.getForObject(url, String.class);
 		ResponseEntity<String> result1 = restTemplate.getForEntity(url, String.class);
 		HttpStatus rescode1 = restTemplate.getForEntity(url, String.class).getStatusCode();
@@ -38,7 +38,35 @@ public class RestTemplateTest {
 	    ResponseEntity<String> result3 = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 	    HttpStatus rescode3 = restTemplate.exchange(url, HttpMethod.GET, entity, String.class).getStatusCode();
 	    
-	    restTemplate.getForObject(url, String.class);
+	    System.out.println("result2 === " + result2); 
+	    System.out.println("result3 === " + result3);
+	    System.out.println("rescode3 === " + rescode3);
+	}
+	
+	@Test
+	public void postTest() {
+		String request = "Test Message";
+		String request1 = "Test Message1";
+		String request2 = "Test Message2";
+		String result = restTemplate.postForObject(url, request, String.class);
+		ResponseEntity<String> result1 = restTemplate.postForEntity(url, request1, String.class);
+		HttpStatus rescode1 = restTemplate.postForEntity(url, request2, String.class).getStatusCode();
+		System.out.println("result = " + result);
+		System.out.println("result1 = " + result1);
+		System.out.println("rescode1 = " + rescode1);
+//		
+		HttpHeaders headers = new HttpHeaders();
+	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+	    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+//	    headers.add("content-type", "application/json;charset=UTF-8");
+	    HttpEntity <String> entity = new HttpEntity<String>(request, headers);
+	    HttpEntity <String> entity1 = new HttpEntity<String>(request1, headers);
+	    HttpEntity <String> entity2 = new HttpEntity<String>(request2, headers);
+	    
+	    String result2 = restTemplate.exchange(url, HttpMethod.POST, entity, String.class).getBody();
+	    ResponseEntity<String> result3 = restTemplate.exchange(url, HttpMethod.POST, entity1, String.class);
+	    HttpStatus rescode3 = restTemplate.exchange(url, HttpMethod.POST, entity2, String.class).getStatusCode();
+	    
 	    System.out.println("result2 === " + result2); 
 	    System.out.println("result3 === " + result3);
 	    System.out.println("rescode3 === " + rescode3);
